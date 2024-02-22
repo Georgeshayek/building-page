4a839032bcf1ab254aeaed0925525609637f6ccf
Georgeshayek <georgeshayek85@gmail.com>
2024-02-22 14:41:21 +0200

    image adding title

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer=()=>{
         <footer className="pt-3 pb-3 text-white bg-[#333]">
         <div className="container mx-auto">
         <div className="flex justify-between items-center p-2 mx-2">
-            <div class="logo">
+            <div className="logo">
                 <Link href="#"><Image src="/logo.jpg" width={50} height={50} alt="Company Logo"/></Link>
             </div>
             <div className="flex max-lg:flex-col">
--- src/components/images.js
+++ src/components/images.js
@@ -2,16 +2,14 @@
 const { default: Image } = require("next/image")
 
 const Images=()=>{
-    const images=['/city1.jpg','/city2.jpeg','/city3.jpg']
+    const images=[{imageurl:'/city1.jpg',title:'city1'},{imageurl:'/city2.jpeg',title:'city2'},{imageurl:'/city3.jpg',title:'city3'}]
     return(
         <div className="container mx-auto">
         <div className="text-center my-6">
             <h1 className="text-4xl my-5"> Images</h1>
         <div className="flex justify-around max-lg:flex-col max-lg:items-center ">
-            {images.map((img,i)=><Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200} src={img} alt={`city${i+1}`} />)}
-			{/* <Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200} src="/city1.jpg" alt="city1" />
-			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200}  src="/city2.jpeg" alt="city2" />
-			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200}  src="/city3.jpg" alt="city3" /> */}
+            {images.map((img,i)=><div className="flex flex-col"><Image className="object-cover mx-4 rounded max-lg:m-2 h-[250px] w-[350px]" width={250} height={200} src={img.imageurl} alt={`city${i+1}`} /><h4>{img.title}</h4></div>)}
+			
 		</div>
         </div>
         </div>
