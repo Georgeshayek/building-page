72a276c665faae88d908dc528275468a0231e7ce
Georgeshayek <georgeshayek85@gmail.com>
2024-02-22 14:33:35 +0200

    image mapping

--- src/components/forminput.js
+++ src/components/forminput.js
@@ -7,13 +7,13 @@ const FormInputs=()=>{
         <h1 className=" text-center text-4xl p-6">Contact us</h1>
 <form className="max-w-[600px] mx-auto my-5" action="/" method="POST">
     <div className="flex flex-col justify-center items-center">
-    <label htmlfor="name">Name:</label>
+    <label htmlFor="name">Name:</label>
     <input type="text" id="name" name="name" className="w-[50%] p-1 rounded-md m-1 border-2 border-slate-400 max-lg:w-[80%]" required/>
 
-    <label htmlfor="email">Email:</label>
+    <label htmlFor="email">Email:</label>
     <input type="email" id="email" name="email" className="w-[50%] p-1 rounded-md m-1 border-2 border-slate-400 max-lg:w-[80%]" required/>
 
-    <label htmlfor="issue">Issue:</label>
+    <label htmlFor="issue">Issue:</label>
     <textarea id="issue" name="issue" rows="8" className="w-[60%] p-1 rounded-lg border-2 border-slate-400 max-lg:w-[80%]" required></textarea>
 
     <button className="bg-white border-2 border-slate-400 block p-3 mt-1 rounded-lg m-auto hover:bg-[#555] hover:text-white hover:cursor-pointer hover:mx-auto max-lg:w-[70%]" type="submit">Submit</button>
--- src/components/images.js
+++ src/components/images.js
@@ -1,14 +1,17 @@
+"use client"
 const { default: Image } = require("next/image")
 
 const Images=()=>{
+    const images=['/city1.jpg','/city2.jpeg','/city3.jpg']
     return(
         <div className="container mx-auto">
         <div className="text-center my-6">
             <h1 className="text-4xl my-5"> Images</h1>
         <div className="flex justify-around max-lg:flex-col max-lg:items-center ">
-			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200} src="/city1.jpg" alt="city1" />
+            {images.map((img,i)=><Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200} src={img} alt={`city${i+1}`} />)}
+			{/* <Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200} src="/city1.jpg" alt="city1" />
 			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200}  src="/city2.jpeg" alt="city2" />
-			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200}  src="/city3.jpg" alt="city3" />
+			<Image className="object-cover mx-4 rounded max-lg:m-2 w-[350px]" width={250} height={200}  src="/city3.jpg" alt="city3" /> */}
 		</div>
         </div>
         </div>
