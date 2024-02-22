708223c92c079fc1400e1b3461264894e4400f01
Georgeshayek <georgeshayek85@gmail.com>
2024-02-22 14:09:33 +0200

    color change v2 link active

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,9 +20,9 @@ const Navbar=()=>{
                 </div>
                 <button id="burger-btn" className="  block text-white lg:hidden" onClick={toggleShow}>&#9776;</button>
                 <ul className={`flex justify-center  max-lg:flex-col ${!show ? 'max-lg:hidden' : 'max-lg:block'} max-lg:absolute max-lg:top-[60px] max-lg:left-0 max-lg:bg-[#333] max-lg:p-1 max-lg:w-[100%] px-[40px] max-lg:z-50`}>                
-				<li className="max-lg:text-center  lg:inline-block lg:mr-[10px]"><Link className={`text-white py-10 px-[10px] hover:text-[#555] ${path==="/"?"text-[#3CB043] font-semibold":""}`} href="/">Home</Link></li>
-				<li className=" max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`text-white py-5 px-[10px] hover:text-[#555] ${path==="/about"?"text-[#3CB043] font-semibold":""}`} href="/about">About</Link></li>
-				<li className="max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`text-white py-5 px-[10px] hover:text-[#555] ${path==="/contact"?"text-[#3CB043] font-semibold":""}`} href="/contact">Contact</Link></li>
+				<li className="max-lg:text-center  lg:inline-block lg:mr-[10px]"><Link className={`py-10 px-[10px] hover:text-[#555] ${path==="/"?"text-green-500 font-semibold":"text-white"}`} href="/">Home</Link></li>
+				<li className=" max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] hover:text-[#555] ${path==="/about"?"text-green-500 font-semibold":"text-white"}`} href="/about">About</Link></li>
+				<li className="max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] hover:text-[#555] ${path==="/contact"?"text-green-500 font-semibold":"text-white"}`} href="/contact">Contact</Link></li>
 			</ul>
             
         </div>
