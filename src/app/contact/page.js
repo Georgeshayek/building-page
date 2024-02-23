cdb2dacf0982ffa13a019c1b71d28901b4862b74
Georgeshayek <georgeshayek85@gmail.com>
2024-02-23 13:17:35 +0200

    added accordion in the contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,4 +1,6 @@
+"use client"
 import Aboutus from "@/components/about";
+import DefAccordion from "@/components/accordion";
 import ImageFiltering from "@/components/imageFiltering";
 
 const { default: HeroImage } = require("@/components/heroImage");
@@ -12,6 +14,7 @@ const Contact = () => {
 				paragraph=" Please contact us for any emergency."
 			/>
 			<Aboutus paragraph="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus," />
+			<DefAccordion/>
 			<ImageFiltering/>
 		</>
 	);
