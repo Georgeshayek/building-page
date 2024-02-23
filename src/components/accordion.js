29e1f07bf72239fd0a8e2fc1424b65af183c6f9e
Georgeshayek <georgeshayek85@gmail.com>
2024-02-23 13:44:27 +0200

    added  responsive accordion in the contact page

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -13,7 +13,7 @@ export function DefAccordion() {
   return (
     <>
       <Accordion open={open === 1} className="p-4 bg-slate-500 text-white">
-        <AccordionHeader className="flex" onClick={() => handleOpen(1)}><p>What is Material Tailwind?</p><p></p><button className="ml-[1000px] text-[1.5rem] w-[50px]  rounded-xl" onClick={() => handleOpen(1)}>{open===1&&<p>-</p>}{open!==1&&<p>+</p>}</button></AccordionHeader>
+        <AccordionHeader className="flex justify-between" onClick={() => handleOpen(1)}><p>What is Material Tailwind?</p><p></p><button className=" ml-[1000px] max-lg:ml-[190px] text-[1.5rem] w-[50px]  " onClick={() => handleOpen(1)}>{open===1&&<p>-</p>}{open!==1&&<p>+</p>}</button></AccordionHeader>
         <AccordionBody className={`${open === 1 ? "" : "text-transparent"}`}>
           We&apos;re not always in the position that we want to be at. We&apos;re constantly
           growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
@@ -21,7 +21,7 @@ export function DefAccordion() {
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 2} className="p-4 bg-slate-500 text-white">
-        <AccordionHeader className="flex" onClick={() => handleOpen(2)}><p>What is Material Tailwind?</p><p></p><button className="ml-[1000px] text-[1.5rem] w-[50px]  rounded-xl" onClick={() => handleOpen(2)}>{open===2&&<p>-</p>}{open!==2&&<p>+</p>}</button></AccordionHeader>
+        <AccordionHeader className="flex justify-between" onClick={() => handleOpen(2)}><p>What is Material Tailwind?</p><p></p><button className=" ml-[1000px] max-lg:ml-[190px] text-[1.5rem] w-[50px]  " onClick={() => handleOpen(2)}>{open===2&&<p>-</p>}{open!==2&&<p>+</p>}</button></AccordionHeader>
         <AccordionBody className={`${open === 2 ? "" : "text-transparent"}`}>
           We&apos;re not always in the position that we want to be at. We&apos;re constantly
           growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
